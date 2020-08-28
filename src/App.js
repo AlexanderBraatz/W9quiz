811f533a53242a761e1db7b18b3930f13071e567
Alexander Braatz <alex.braatz.g@gmail.com>
2020-08-28 15:31:30 +0100

    made CountBy

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Multiplier  from './components/Multiplier';
 import EvenClicks  from './components/EvenClicks';
+import CountBy  from './components/CountBy';
 
 function App() {
   return (
     <div className="App container">
       <Multiplier x= { 2 } y= { 3 }/>
       <EvenClicks />
+      <CountBy step= { 5 }/>
     </div>
   );
 }
---
+++ src/components/CountBy.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class CountBy extends Component {
+
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+          count: 0,
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+         count: this.state.count + this.props.step,
+        });
+    }
+
+    render(){
+        return(
+            <p className = "lead card mb-3" onClick= { this.handleClick } >
+            { this.state.count }
+            </p>
+        )
+    }
+}
+
+
+export default CountBy ; 
